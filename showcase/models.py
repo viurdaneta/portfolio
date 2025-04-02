52276cbecf2d5a6f40065d3347d9dfa7e5e53794
Victor Urdaneta <vurdaneta@Victors-MacBook-Air.local>
2025-04-02 14:44:39 -0400

    Created models and templates for showcase app

---
+++ showcase/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=30)
+    tools = models.CharField(max_length=200)
+    url = models.URLField()
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='project_images/')
