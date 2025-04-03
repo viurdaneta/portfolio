513ede1d716403a18f42d6e7398f1ee72acef16d
Victor Urdaneta <vurdaneta@Victors-MacBook-Air.local>
2025-04-03 19:28:25 -0400

    Home page and part of contacts page

--- showcase/models.py
+++ showcase/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=30)
-    description = models.TextField(max_length=30)
+    description = models.TextField()
     tools = models.CharField(max_length=200)
     url = models.URLField()
 class ProjectImage(models.Model):
--- showcase/views.py
+++ showcase/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'showcase/index.html')
+
+def contact(request):
+    return render(request, 'showcase/contact.html')
