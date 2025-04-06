1e5d0464082671b3bc8d940260acd70d97c8604f
Victor Urdaneta <vurdaneta@Victors-MacBook-Air.local>
2025-04-06 12:49:29 -0400

    Settings change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,8 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional but safe:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'showcase/static'),
+]
 
 
 # Default primary key field type
